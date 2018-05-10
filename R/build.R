5bd8d83c251bc55cd66b052abf451175428362ee
Alex Hurley <the-Hull@users.noreply.github.com>
2018-05-10 23:35:03 +0200

    test custom rmd

---
+++ R/build.R
@@ -0,0 +1,14 @@
+blogdown::build_dir('static')
+
+
+# # set common options ofr knitr
+# knitr::opts_knit$set(
+#       base.dir = normalizePath("static/", mustWork = TRUE),
+#       base.url = "/"
+# )
+
+# knitr::opts_chunk$set(
+#       cache.path = normalizePath("cache/", mustWork = TRUE),
+#       collapse = TRUE,
+#       comment  = "#>"
+# )
